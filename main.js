945a60eea882b1cd90c56f94c1ff0fbd4eea94c9
KimGyeongwon <ruddnjs6651@gmail.com>
2024-08-16 16:44:13 +0900

    App-함수를 이용해서 정리정돈(refactoring)까지

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+
+var templateHTML = (title, list, body) => {
+  return `
+          <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          ${body}
+        </body>
+        </html>
+          `;
+};
+
+var templateList = (fileList) => {
+  var list = "<ul>";
+  var i = 0;
+  while (i < fileList.length) {
+    list = list + `<li><a href="?id=${fileList[i]}">${fileList[i]}</a></li>`;
+    i += 1;
+  }
+  list = list + "</ul>";
+  return list;
+};
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -8,55 +37,35 @@ var app = http.createServer(function (request, response) {
 
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+      fs.readdir("./data", function (error, fileList) {
         var title = "Welcome";
         var description = "Hello, Node.js";
-        var template = `
-        <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-        `;
+        var list = templateList(fileList);
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
         response.writeHead(200);
         response.end(template);
       });
     } else {
-      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-        var title = queryData.id;
-        var template = `
-        <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
+      fs.readdir("./data", function (error, fileList) {
+        fs.readFile(
+          `data/${queryData.id}`,
+          "utf8",
+          function (err, description) {
+            var title = queryData.id;
+            var list = templateList(fileList);
+            var template = templateHTML(
+              title,
+              list,
+              `<h2>${title}</h2>${description}`
+            );
+            response.writeHead(200);
+            response.end(template);
+          }
+        );
       });
     }
   } else {
